6613dfa95b86bb7854fef9e94606bd40c0430de7
abdurDaily <test@gmail.com>
2024-08-18 17:39:35 +0600

    client -1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('Backend.Layout.Layout');
+
+@section('backend_contains')
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt consectetur praesentium corporis enim iure explicabo aliquam odit nihil, amet quibusdam provident quasi. Consectetur harum iusto ipsum aspernatur aliquid veniam eos.</p>
+@endsection
